2db20b19439f3bd8e2361e0f095f454b10489d96
isalpaisal040@gmail.com <isalpaisal040@gmail.com>
2024-01-29 10:17:45 +0700

    ganti

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import{
-getFirestore,
-collection,
-addDoc,
-getDocs.
-deleteDoc,
-doc,
-query,
-orderBy
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy
 } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlahoJjeK0jyO-4tZlAiPRjym6Mxn2P6o",
-  authDomain: "insan-cemerlang-59727.firebaseapp.com",
-  projectId: "insan-cemerlang-59727",
-  storageBucket: "insan-cemerlang-59727.appspot.com",
-  messagingSenderId: "839220708273",
-  appId: "1:839220708273:web:4d1dde85cf74aebd1d7390",
-  measurementId: "G-1VP3D59R0T"
+  apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
+  authDomain: "insan-cemerlang.firebaseapp.com",
+  projectId: "insan-cemerlang",
+  storageBucket: "insan-cemerlang.appspot.com",
+  messagingSenderId: "579109661574",
+  appId: "1:579109661574:web:4a7cd4060f70eded945a07"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export async function ambilDaftarsiswa(){
+export async function ambilDaftarSiswa () {
   const siswaRef = collection(db, "siswa");
-  const q = query(siswaRef,orderBy)("nama"));
-  const queryySnapshot = await getDocs(q);
+  const q = query(siswaRef, orderBy("Nama"));
+  const querySnapshot = await getDocs(q);
   
   let retval = [];
   querySnapshot.forEach((doc) => {
-    retval.push({ id:doc.id, nama:doc.data().nama });
-    });
-    
-    return retval;
-  }
+    retval.push({ id: doc.id, Nama: doc.data().Nama });
+  });
+  
+  return retval;
+}
