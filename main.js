0e60f74d1bfaf525eca32571378e61ac3c8e53f6
isalpaisal040@gmail.com <isalpaisal040@gmail.com>
2024-02-05 10:43:02 +0700

    yana cabul

--- main.js
+++ main.js
@@ -46,6 +46,6 @@ export async function tambahsiswa(nama){
   });
   console.log('Berhasil menyimpan dokumen dengan ID:' + docRef.id);
  } catch (e) {
-   console.log('Error menambah dokumen:'+e);
+   console.log('Error menambah dokumen'+e);
   }
 }
